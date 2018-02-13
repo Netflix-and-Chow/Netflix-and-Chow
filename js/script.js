247ee092f2e3711cedcfd3fe4313518b0d77f9d3
Ryan Wong <ryan.wy.wong88@gmail.com>
2018-02-13 18:35:42 -0500

    JS changes

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//namespace variables
+const netflixChow = {};
+
+//get data from API's
+netflixChow.getData = () => {
+
+}
+
+//manipulate data from API's
+netflixChow.sortData = () => {
+
+}
+
+//display sorted data
+netflixChow.displayData = () => {
+
+}
+
+//events function
+netflixChow.events = () = {
+
+}
+
+//initialization function
+netflixChow.init = () => {
+
+}
+
+//document ready function
+$(function() {
+
+});
