7bdedc837212cf1d9d44aeff46867233b9f01001
Christina Laba <christinalaba.m@gmail.com>
2018-02-15 11:19:15 -0500

    random changes

--- js/script.js
+++ js/script.js
@@ -1,62 +1,62 @@
 //namespace variables
 const netflixChow = {};
 
-// netflixChow.animationMovies = [];
-// netflixChow.actionMovies = [];
-// netflixChow.comedyMovies = [];
-// netflixChow.horrorMovies = [];
-// netflixChow.romanceMovies = [];
-// netflixChow.SFMovies = [];
-// netflixChow.westernMovies = [];
+netflixChow.animationMovies = [];
+netflixChow.actionMovies = [];
+netflixChow.comedyMovies = [];
+netflixChow.horrorMovies = [];
+netflixChow.romanceMovies = [];
+netflixChow.SFMovies = [];
+netflixChow.westernMovies = [];
 
-// //get data from API's
-// //Genre Ids (Action - 28, Adventure - 12, Animation - 16, Comedy - 35, Crime - 80, Documentary - 99, Drama - 18, Family - 10751)
-// //Genre Ids (Fantasy - 14, History - 36, Horror - 27, Music - 10402, Mystery - 9648, Romance - 10749, Science Fiction - 878)
-// //Genre Ids (TV Movie - 10770, Thriller - 53, War - 10752, Western - 37)
-// netflixChow.getMovieData = () => {
-//     $.ajax({
-//         url: "https://api.themoviedb.org/3/discover/movie",
-//         method: "GET",
-//         dataType: "jsonp",
-//         data: {
-//             api_key: "82927986e43b7cf24d8d6e29aacb0e5c",
-//             with_genres:"28|16|35|878|27|10749|37",
-//             language:"en-US"
-//         }
-//     }).then((res) => {
-//         console.log(res);
-//         netflixChow.sortData(res.results);
-//     });
-// }
+//get data from API's
+//Genre Ids (Action - 28, Adventure - 12, Animation - 16, Comedy - 35, Crime - 80, Documentary - 99, Drama - 18, Family - 10751)
+//Genre Ids (Fantasy - 14, History - 36, Horror - 27, Music - 10402, Mystery - 9648, Romance - 10749, Science Fiction - 878)
+//Genre Ids (TV Movie - 10770, Thriller - 53, War - 10752, Western - 37)
+netflixChow.getMovieData = () => {
+    $.ajax({
+        url: "https://api.themoviedb.org/3/discover/movie",
+        method: "GET",
+        dataType: "jsonp",
+        data: {
+            api_key: "82927986e43b7cf24d8d6e29aacb0e5c",
+            with_genres:"28|16|35|878|27|10749|37",
+            language:"en-US"
+        }
+    }).then((res) => {
+        console.log(res);
+        netflixChow.sortData(res.results);
+    });
+}
 
-// // netflixChow.getMovieData();
+netflixChow.getMovieData();
 
-// //sorting data accoding to genre type and filtering them into separate arrays
-// netflixChow.sortData = (movieData) => {
-//     netflixChow.animationMovies = movieData.filter((value) => {
-//         if(value.genre_ids.includes(16)) {
-//             return value;
-//         }
-//     });
-//     console.log(netflixChow.animationMovies);
+//sorting data accoding to genre type and filtering them into separate arrays
+netflixChow.sortData = (movieData) => {
+    netflixChow.animationMovies = movieData.filter((value) => {
+        if(value.genre_ids.includes(16)) {
+            return value;
+        }
+    });
+    console.log(netflixChow.animationMovies);
 
-//     netflixChow.comedyMovies = movieData.filter((value) => {
-//         if (value.genre_ids.includes(35)) {
-//             return value;
-//         }
-//     });
-//     console.log(netflixChow.comedyMovies);
-// }
+    netflixChow.comedyMovies = movieData.filter((value) => {
+        if (value.genre_ids.includes(35)) {
+            return value;
+        }
+    });
+    console.log(netflixChow.comedyMovies);
+}
 
-// //display sorted data
-// netflixChow.displayData = () => {
+//display sorted data
+netflixChow.displayData = () => {
 
-// }
+}
 
-// //events function
-// netflixChow.events = () => {
+//events function
+netflixChow.events = () => {
 
-// }
+}
 
 
 //Get data notes:
@@ -67,8 +67,6 @@ const netflixChow = {};
 
 
 
-
-
 //YUMMLY API:
 // get ingredients
 // get image
@@ -76,9 +74,6 @@ const netflixChow = {};
 // generate 5 recipies
 
 
-
-
-//retrieve the ids from the searcable ingreedient
 netflixChow.getId= (ingredient) => {
     $.ajax({
         url: 'http://api.yummly.com/v1/api/recipes',
@@ -111,11 +106,10 @@ netflixChow.getId= (ingredient) => {
     })
 }
 
+// netflixChow.ingredientMatches[getRandomNumbers.map]
 
 
 
-//create a new array of results
-// pick random selction of ids from this array
 
 //selcted ids will be passed to getRecepie function
 // retrive image and url of each id
